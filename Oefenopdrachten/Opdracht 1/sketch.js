18068dd48bf76b42c909c2755fd7a964e8cc27c0
H <hanad.mohamed055@gmail.com>
2026-09-07 11:19:03 +0100

    I cooked
    9/7/2026

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,38 @@
-function setup() {
-  createCanvas(400, 400);
+function setup() {//--> setup is the function that is called once at the beginning
+  createCanvas(1920, 1080);//--> createCanvas is the size of the canvas
 }
 
-function draw() {
-  background(220);
+function draw() { //--> draw is the function that is called every frame
+  
+  background(220);//--> background is the color of the canvas
+  
+  line(100000000, 100, 400, 300);//--> line is the line that is drawn on the canvas
+  
+  stroke(8, 8, 2);//--> stroke is the color of the line
+  
+  strokeWeight(8, 8, 2); //--> strokeweight is the thickness of the line
+  
+  line(100, 100, 400, 300);//--> line is the line that is drawn on the canvas
+
+  rect(100, 100, 400, 300);//--> rect is the rectangle that is drawn on the canvas
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
