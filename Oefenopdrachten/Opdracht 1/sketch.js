eee58c2bfbd3e90590dde26ad202265439ec7126
H <hanad.mohamed055@gmail.com>
2026-09-15 14:22:10 +0100

    oefenopdracht 2
    gemaakt

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -4,35 +4,98 @@ function setup() {//--> setup is the function that is called once at the beginni
 
 function draw() { //--> draw is the function that is called every frame
   
-  background(220);//--> background is the color of the canvas
-  
-  line(100000000, 100, 400, 300);//--> line is the line that is drawn on the canvas
-  
-  stroke(8, 8, 2);//--> stroke is the color of the line
-  
-  strokeWeight(8, 8, 2); //--> strokeweight is the thickness of the line
-  
-  line(100, 100, 400, 300);//--> line is the line that is drawn on the canvas
+  background(220);
 
-  rect(100, 100, 400, 300);//--> rect is the rectangle that is drawn on the canvas
-  
+  fill("Blue")
+  rect(30, 20, 90, 20, 0, 0,);//-- 1
 
+  fill("White")
+  rect(100, 20, 55, 20, 0, 0,);//2
 
-}
+  fill("white")
+  rect(155, 20, 55, 20, 0, 0,);//3
 
+  fill("White")
+  rect(210, 20, 90, 20, 0, 0,);//4
 
+  fill("yellow")
+  rect(300, 20, 35, 50, 0, 0,);//5
 
+  fill("White")
+  rect(300, 70, 35, 35, 0, 0,);//6
 
+  fill("Black")
+  rect(336, 20, 35, 29, 0, 0,);//7
 
+  fill("White")
+  rect(336, 50, 35, 70, 0, 0,);//8
 
+  fill("Red")
+  rect(336, 120, 35, 70, 0, 0,);//9
 
+  fill("White")
+  rect(336, 190, 35, 44, 0, 0,);//10
 
+  fill("Blue")
+  rect(336, 235, 35, 20, 0, 0,);//11
 
+  fill("White")
+  rect(300, 190, 35, 65, 0, 0,);//12
 
+  fill("White")
+  rect(300, 155, 35, 35, 0, 0,);//13
 
+  fill("Blue")
+  rect(300, 105, 35, 49, 0, 0,);//14
 
+  fill("White")
+  rect(210, 105, 90, 46, 0, 0,);//15
 
+  fill("Blue")
+  rect(210, 151, 90, 46, 0, 0,);
 
+  fill("Yellow")
+  rect(254, 195, 45, 44, 0, 0,);
 
+  fill("White")
+   rect(210, 195, 45, 44, 0, 0,);
 
+  fill("Black")
+  rect(210, 240, 90, 20, 0, 0,);
 
+  fill("White")
+  rect( 164, 240, 46, 20, 0, 0,);
+
+  fill("White")
+  rect( 118, 240, 46, 20, 0, 0,);
+
+  fill("red")
+  rect( 37, 240, 80, 20, 0, 0,);
+
+  fill("Blue")
+  rect( 37, 189, 80, 50, 0, 0,);
+
+  fill("green")
+  rect( 118, 189, 91, 50, 0, 0,);
+
+  fill("orange")
+  rect( 118, 139, 91, 50, 0, 0,);
+
+  fill("orange")
+  rect( 206, 40, 99, 64, 0, 0,); // loool 50 duizend
+
+  fill("cyan")
+  rect( 118, 89, 91, 50, 0, 0,);
+
+  fill("purple")
+  rect( 118, 40, 91, 50, 0, 0,);
+
+  fill("grey")
+  rect( 26, 40, 91, 50, 0, 0,);
+
+  fill("pink")
+  rect(26, 91, 91, 50, 0, 0,);
+
+  fill("yellow")
+  rect(26, 142, 91, 50, 0, 0,);
+} 
--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,53 @@
+let a = 40;
+let b = 300;
+
+let kaas = 30
+let iamcoding = 24
+let learned = 2000
+let iksnaphet = 300
+
+
+let som = a + b;
+let verschil = a - b;
+let product = a * b;
+let uitkomst = a / b;
+let rest = a % b;
+let Hanad = iamcoding + b
+let ja = learned * b
+let hetisgoed = iksnaphet + b
+
 function setup() {
   createCanvas(400, 400);
 }
 
+
+
 function draw() {
-  background(220);
+  background(0, 0, 0);
+  
+  fill("yellow")
+  textSize(50)
+  text("iksnaphet = " + (hetisgoed + b), 40, 120)
+  
+  
+  fill("green")
+  textSize(30)
+  text("learned ja * b =" + (ja * b), 30, 80);
+  
+  fill("Pink")
+  textSize(25);
+  text("iamcoding = " + (iamcoding + b), 30, 45);
+  
+  fill("Red")
+  textSize(50)
+  text("som : = " + (a + b),   40, 250);// dit is een som a+b
+  
+  fill("Cyan")
+  textSize(25)
+  text("verschil: = " + (a - b), 50, 315);// dit is een min som a-b
+  
+  fill("Purple")
+  textSize(24)
+  text("product = " + (a * b), 50, 350);
 }
+
