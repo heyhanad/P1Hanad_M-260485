a09e319473d2a287c21c53deee14c7024ae5ebfb
H <hanad.mohamed055@gmail.com>
2026-09-16 10:41:37 +0100

    Update sketch.js

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,18 @@
+if(score > 80 && score <= 90) {
+  console.log("je score is tussen 80 en 90");
+}
+
+
+
+
+
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  
 }
