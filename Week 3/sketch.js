e2e3d2f3d460e697a59b4ce43ba0ab256d60a2c3
H <hanad.mohamed055@gmail.com>
2026-09-25 09:59:18 +0100

    week 3 boter & eiren upd
    updated

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,19 +1,40 @@
 let gameStart = "start"; // een variable voor spel start
+// dit zijn de varb voor de x/y vakken
 let vak1X = 205;
 let vak1Y = 110;
-let vak2X = 322
-let vak2Y = 110
-let vak3X = 446
-let vak3Y = 110
+let vak2X = 322;
+let vak2Y = 110;
+let vak3X = 446;
+let vak3Y = 110;
+let vak4X = 440;
+let vak4Y = 240;
+let vak5X = 205;
+let vak5Y = 240;
+let vak6X = 320;
+let vak6Y = 240;
+let vak7X = 320;
+let vak7Y = 350;
+let vak8X = 205;
+let vak8Y = 350;
+let vak9X = 440;
+let vak9Y = 350;
+
 
 
 let vakSize = 90;
 let vakRadius = 30;
-
+// vaken
 let vak1 = 0;
 let vak2 = 0;
 let vak3 = 0;
+let vak4 = 0;
+let vak5 = 0;
+let vak6 = 0;
+let vak7 = 0;
+let vak8 = 0;
+let vak9 = 0;
 
+// speler 1 heeft blauw / speler 2 heeft rood
 let speler = 2;
 
 
@@ -21,15 +42,15 @@ let speler = 2;
 async function setup() {
   createCanvas(800, 800);
   font = await loadFont('/assets/inconsolata.otf');
-  //  dit is de font
+  //  dit is de font = text
 }
+
 
 function mousePressed() {
   if (gameStart == "start") {
     gameStart = "spel";
   }  // game start wannaeeer ik op mijn muis click
 }
-
 
 function draw() {
   background(220);
@@ -44,12 +65,6 @@ function draw() {
   square(200, 100, 360, 20) // dit is het square waar de spel op wordt gespeeld
 
   fill("grey")
-  square(440, 240, 90, 30);// vak 4 // midden rechts
-  square(205, 240, 90, 30);// vak 5 midden links
-  square(320, 240, 90, 30);// vak 6 midden 
-  square(320, 350, 90, 30); // vak 7 midden onder
-  square(205, 350, 90, 30);// vak 8 links onder
-  square(440, 350, 90, 30);// vak 9 rechts onder
   // dit zijn de vakken die ik heb gemaakt en,
   // die ik ook later nodig heb om te zien waar elke vak is
 
@@ -87,6 +102,73 @@ function draw() {
   }
   square(vak3X, vak3Y, vakSize, vakRadius);// vak 3 boven rechts
 
+  if (vak4 == 0) {
+    fill("grey");
+  }
+  else if (vak4 == 1) {
+    fill("blue");
+  }
+  else if (vak4 == 2) {
+    fill("red");
+  }
+  square(vak4X, vak4Y, vakSize, vakRadius);// vak 4 // midden rechts
+
+  if (vak5 == 0) {
+    fill("grey");
+  }
+  else if (vak5 == 1) {
+    fill("blue");
+  }
+  else if (vak5 == 2) {
+    fill("red");
+  }
+  square(vak5X, vak5Y, vakSize, vakRadius);// vak 5 midden links
+
+  if (vak6 == 0) {
+    fill("grey");
+  }
+  else if (vak6 == 1) {
+    fill("blue");
+  }
+  else if (vak6 == 2) {
+    fill("red");
+  }
+  square(vak6X, vak6Y, vakSize, vakRadius);// vak 6 midden 
+
+  if (vak7 == 0) {
+    fill("grey")
+  }
+  else if (vak7 == 1) {
+    fill("blue")
+  }
+  else if (vak7 == 2) {
+    fill("red")
+  }
+  square(vak7X, vak7Y, vakSize, vakRadius); // vak 7 midden onder
+
+  if (vak8 == 0) {
+    fill("grey")
+  }
+  else if (vak8 == 1) {
+    fill("blue")
+  }
+  else if (vak8 == 2) {
+    fill("red")
+  }
+  square(vak8X, vak8Y, vakSize, vakRadius);// vak 8 links onder
+
+
+  if (vak9 == 0) {
+    fill("grey")
+  }
+  else if (vak9 == 1) {
+    fill("blue")
+  }
+  else if (vak9 == 2) {
+    fill("red")
+  }
+  square(vak9X, vak9Y, vakSize, vakRadius);// vak 9 rechts onder
+
   fill("cyan")
   circle(mouseX, mouseY, 50);
   // dit is die grappige blauw circle om te zien waar je mouse is
@@ -105,7 +187,7 @@ function draw() {
 
 function mousePressed() {
 
-  if (mouseX > vak1X && mouseX < vak1X + vakSize) {
+  if (mouseX > vak1X && mouseX < vak1X + vakSize && vak1 == 0) {
     if (mouseY > vak1Y && mouseY < vak1Y + vakSize) {
       vak1 = speler;
 
@@ -118,7 +200,7 @@ function mousePressed() {
     }
   }
 
-  if (mouseX > vak2X && mouseX < vak2X + vakSize) {
+  if (mouseX > vak2X && mouseX < vak2X + vakSize && vak2 == 0) {
     if (mouseY > vak2Y && mouseY < vak2Y + vakSize) {
       vak2 = speler;
 
@@ -131,7 +213,7 @@ function mousePressed() {
     }
   }
 
-  if (mouseX > vak3X && mouseX < vak3X + vakSize) {
+  if (mouseX > vak3X && mouseX < vak3X + vakSize && vak3 == 0) {
     if (mouseY > vak3Y && mouseY < vak3Y + vakSize) {
       vak3 = speler;
 
@@ -144,4 +226,89 @@ function mousePressed() {
     }
   }
 
+  if (mouseX > vak4X && mouseX < vak4X + vakSize && vak4 == 0) {
+    if (mouseY > vak4Y && mouseY < vak4Y + vakSize) {
+      vak4 = speler;
+
+      speler = speler + 1;
+
+      if (speler > 2) {
+        speler = 1
+      }
+
+    }
+  }
+
+  if (mouseX > vak5X && mouseX < vak5X + vakSize && vak5 == 0) {
+    if (mouseY > vak5Y && mouseY < vak5Y + vakSize) {
+      vak5 = speler;
+
+      speler = speler + 1;
+
+      if (speler > 2) {
+        speler = 1
+      }
+
+    }
+  }
+
+  if (mouseX > vak6X && mouseX < vak6X + vakSize && vak6 == 0) {
+    if (mouseY > vak6Y && mouseY < vak6Y + vakSize) {
+      vak6 = speler;
+
+      speler = speler + 1;
+
+      if (speler > 2) {
+        speler = 1
+      }
+
+    }
+  }
+
+  if (mouseX > vak7X && mouseX < vak7X + vakSize && vak7 == 0) {
+    if (mouseY > vak7Y && mouseY < vak7Y + vakSize) {
+      vak7 = speler;
+
+      speler = speler + 1;
+
+      if (speler > 2) {
+        speler = 1
+      }
+
+    }
+  }
+
+
+  if (mouseX > vak8X && mouseX < vak8X + vakSize && vak8 == 0) {
+    if (mouseY > vak8Y && mouseY < vak8Y + vakSize) {
+      vak8 = speler;
+
+      speler = speler + 1;
+
+      if (speler > 2) {
+        speler = 1
+      }
+
+    }
+  }
+
+  if (mouseX > vak9X && mouseX < vak9X + vakSize && vak9 == 0) {
+    if (mouseY > vak9Y && mouseY < vak9Y + vakSize) {
+      vak9 = speler;
+
+      speler = speler + 1;
+
+      if (speler > 2) {
+        speler = 1
+      }
+
+    }
+  }
+
+  if (vak1 == 1 && vak2 == 1 && vak3 == 1) {
+     fill("white")
+    text("BLUE WINS!", 350, 230, 90)
+  }
+  
 }
+
